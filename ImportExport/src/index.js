bceb72eab9cf406f61c771e8a0a27a5f345ee7e4
Surik95 <pokemon.surik@gmail.com>
2022-10-02 16:57:11 +0400

    выполнил дз по теме 2

---
+++ ImportExport/src/index.js
@@ -0,0 +1,1 @@
+import result from './js/app.js';
---
+++ ImportExport/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlagins = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js', //точка входа отличная от стандартной
+  output: { path: path.resolve(__dirname, 'dist'), filename: 'app.bundle.js' }, // сохранинени отличного от стандартного
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.txt$/,
+        loader: 'raw-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlagins({
+      template: 'src/index.html',
+      filename: 'main.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
